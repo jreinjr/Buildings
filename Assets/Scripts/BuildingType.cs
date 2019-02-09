41618b767830f8e7dd3b142582bf55400a8f6878
Jason Reinhardt <jason.w.reinhardt@gmail.com>
2019-02-09 11:43:17 -0800

    190209
    First commit

---
+++ Assets/Scripts/BuildingType.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Building", menuName = "Buildings/BuildingType", order = 1)]
+public class BuildingType : ScriptableObject
+{
+    public string description;
+    public char hotkey;
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Singleton instance
+    public static GameManager instance = null;
+
+    
+    private void Awake()
+    {
+        // Singleton behavior
+        if (instance == null) instance = this;
+        else if (instance != this) DestroyImmediate(gameObject);
+    }
+
+
+    private void Update()
+    {
+        
+    }
+}
